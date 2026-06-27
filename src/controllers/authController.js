d0888e7b52c3d511fb4876f287385b7cdeeffbd7
komandor <koletttut@gmail.com>
2026-06-27 17:17:23 +0300

    fix: enforce sessionId check in auth middleware, compound index on note schema, and cleanup on expired refresh token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -69,6 +69,10 @@ export const refreshUserSession = async (req, res, next) => {
 
     const isExpired = new Date() > new Date(session.refreshTokenValidUntil);
     if (isExpired) {
+      await Session.deleteOne({ _id: sessionId });
+      res.clearCookie('accessToken', { httpOnly: true, secure: true, sameSite: 'none' });
+      res.clearCookie('refreshToken', { httpOnly: true, secure: true, sameSite: 'none' });
+      res.clearCookie('sessionId', { httpOnly: true, secure: true, sameSite: 'none' });
       throw createHttpError(401, 'Session token expired');
     }
 
